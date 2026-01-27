0709bb36b634b6835a0fa18915649d706a478f1f
Abhimanyu Pratap Singh <abhimanyu2004111@gmail.com>
2026-01-28 01:09:10 +0530

    Update food routes and middleware

--- backend/src/controller/food.controler.js
+++ backend/src/controller/food.controler.js
@@ -1,9 +1,11 @@
-// import foodModel from "../models/food.model.js";
+import foodModel from "../models/food.model.js";
 
 export async function createFood(req, res) {
     try {
         // You can now access the foodPartner attached by your middleware
         console.log("Creating food for partner:", req.foodPartner);
+        console.log(req.body);
+        console.log(req.file);
         
         // Example logic to save to database
         // const newFood = await foodModel.create({ ...req.body, foodPartner: req.foodPartner._id });
--- backend/src/routes/food.routes.js
+++ backend/src/routes/food.routes.js
@@ -3,11 +3,17 @@ import express from 'express';
 import { createFood } from '../controller/food.controler.js';
 // Use curly braces {} because auth.middleware.js uses a named export
 import { authFoodPartnerMiddleware } from "../middlewares/auth.middleware.js";
+// import {authMiddleware} from '../middlewares/auth.middleware.js';
+import multer from 'multer';
 
+const upload  = multer({
+    storage: multer.memoryStorage(),
+})
 const router = express.Router();
 
-// 1. Ensure it is .post (not .postt)
-// 2. Use the correct middleware function name
-router.post("/", authFoodPartnerMiddleware,createFood);
+router.post('/',
+    authFoodPartnerMiddleware,
+    upload.single("video"), createFood
+);
 
 export default router;
