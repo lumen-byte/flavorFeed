5e517cfa8e40dc641bdcebf3d6bfda736e2e8ca2
Abhimanyu <abhimanyu2004111@gmail.com>
2026-05-01 19:38:37 +0530

    fix: vercel runtime errors and db connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import connectDB from './db/db.js';
 import dotenv from 'dotenv';
 import http from 'http';
 import { initializeSocket } from './socket.js';
+
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,10 @@ connectDB();
 const server = http.createServer(app);
 export const io = initializeSocket(server);
 
-server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (!process.env.VERCEL) {
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+export default app;
